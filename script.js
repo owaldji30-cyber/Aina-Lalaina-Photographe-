489332aec55d60661ac2c443ca7f6ba4a0fbc5e0
owaldji30-cyber <owaldji30@gmail.com>
2026-09-16 23:29:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // CONFIGURATION API
 // =========================
 
-const API_URL =
+const API_URL =const API_URL =
     "https://script.google.com/macros/s/AKfycbwmn15NAR7v8w-6Ikpxy1HpeK4adxROxSx3N41WJZOG0V4xlkAni5R7Xi0qvp-hXNWz/exec";
 
 // Charger automatiquement les événements
@@ -24,6 +24,7 @@ async function chargerEvenements() {
 
         if (!data.success) {
             throw new Error("Erreur dans les données");
+            
         }
 
         afficherEvenements(data.events);
